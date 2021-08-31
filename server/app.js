8b39e9f4db64498aa3da8965868bced070f5403a
asimmaqsood57 <asimmaqsood57@gmail.com>
2021-08-31 07:36:41 +0500

    backend is working fine and ready

--- server/app.js
+++ server/app.js
@@ -1,15 +1,17 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
-
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 
+const cookieparser = require("cookie-parser");
 const app = express();
 
 const userMod = require("./modles/users");
 app.use(express.json());
 
 app.use(cors());
+app.use(cookieparser());
 
 app.get("/", (req, res) => {
   res.send("welcome to homepage");
@@ -30,6 +32,21 @@ mongoose
     console.log("Something went wrong", err);
   });
 
+const authorization = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (!token) {
+    return res.sendStatus(403);
+  }
+  try {
+    const data = jwt.verify(token, "YOUR_SECRET_KEY");
+    req.userId = data.id;
+    req.userRole = data.role;
+    return next();
+  } catch {
+    return res.sendStatus(403);
+  }
+};
+
 app.post("/register", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
@@ -48,6 +65,18 @@ app.post("/register", async (req, res) => {
     console.log(error);
   }
 });
+
+app.get("/protected", authorization, (req, res) => {
+  return res.json({ user: { id: req.userId, role: req.userRole } });
+});
+
+app.get("/logout", authorization, (req, res) => {
+  return res
+    .clearCookie("access_token")
+    .status(200)
+    .json({ message: "Successfully logged out 😏 🍀" });
+});
+
 app.post("/login", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
@@ -57,8 +86,17 @@ app.post("/login", async (req, res) => {
   console.log(user);
   try {
     if (password == user.password) {
-      console.log("you are signed in");
-      res.send("You are  signed in successfully");
+      const token = jwt.sign(
+        { id: user._id, role: "captain" },
+        "YOUR_SECRET_KEY"
+      );
+      return res
+        .cookie("access_token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+        })
+        .status(200)
+        .json({ message: "Logged in successfully 😊 👌" });
     }
   } catch (error) {
     console.log(error);
